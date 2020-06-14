bdf3d0eea98c90d7907d9b95b70d8ff36e428305
Goodman Luphondo <luphondog@outlook.com>
2020-06-14 13:41:19 +0200

    Corrected About Page

--- about.php
+++ about.php
@@ -22,34 +22,46 @@ include("includes/header.php");
                 <h2>About Us</h2>
                 <p>Life Restorers (NPO) was born from TACTS Foundation (NPO) that was founded in 2009. The organisation was founded in year 2009 in Carletonville, South Africa. The project was initially aimed at vulnerable and needy children of human beings who came to seek work or are working at the mines, residing in informal settlements. It was in latter extended the focus to any vulnerable child. The primary focus of the project was and is to assist needy children get ahead and make success of life through all the circumstances and challenges they are faced with.</p>
                 <p>Life Restorers focus not just on children or youth, but on that nation in entirety. This is due to a realisation that the nation as a whole needs building, and there are interdependencies that inclusively focus on, to create growth and success in the nation. Our aim is to create possibilities and provide chances for positive change and transformation of our people.</p>
-
-                <h4>Vision</h4>
-                <p>To emancipate and empower the nation through restoration and building together.</p>
-
-                <h4>Mission</h4>
-                <ul>
-                    <li>Ensure people, youth and old have access to essential resources for self-development and success</li>
-                    <li>Ensure access to quality education that complements the education system in the country and the world</li>
-                    <li>Each child having understanding and access to basic and fundamental    educational needs</li>
-                    <li>Re-engineering mind-sets on leadership and reinstating the positive outlook on learning and education.</li>
-                    <li>Skills development to empower people for employment, entrepreneurship and job creation
-                    <li>Access to employment, including learnerships and internships </li>
-                    <li>Economic development to capacitate and empower SMMEs, NPOs, NGOs and Entrepreneurs</li>
-                    <li>Social development through reinstating sense support, giving and working together as a community.</li>
-                </ul>
-                <h4>Impact</h4>
-                <ul>
-                    <li>We provide a connection between the people in need and the resources they need to make it and succeed in life</li>
-                    <li>We partner with the Government and Organisations to ensure effective positive impact on our nation</li>
-                    <li>We provide complementary education necessary for self-development and leadership in the nation</li>
-                    <li>We equip the nation with essential skills to thrive in the economy, in turn create jobs and employment</li>
-                    <li>Access to employment, including learnerships and internships </li>
-                    <li>We create an environment of social development where there is support, giving back and empowerment of all, especially the vulnerable and previously disadvantaged</li>
-                    <li>We capacitate and empower SMMES, NPOs, NGOs and Entrepreneurs with necessary support to succeed and thrive with the economy</li>
-                </ul>
             </div><!-- end of .col -->
             <div class="col col-6">
                 <img src="assets/about-hands.jpg"/>
+            </div><!-- end of .col -->
+        </div><!-- end of .container -->
+        <div class="container">
+            <div class="col col-4">
+                <header class="section-header">
+                    <h4>Mission</h4>
+                    <ul>
+                        <li>Ensure people, youth and old have access to essential resources for self-development and success</li>
+                        <li>Ensure access to quality education that complements the education system in the country and the world</li>
+                        <li>Each child having understanding and access to basic and fundamental    educational needs</li>
+                        <li>Re-engineering mind-sets on leadership and reinstating the positive outlook on learning and education.</li>
+                        <li>Skills development to empower people for employment, entrepreneurship and job creation
+                        <li>Access to employment, including learnerships and internships </li>
+                        <li>Economic development to capacitate and empower SMMEs, NPOs, NGOs and Entrepreneurs</li>
+                        <li>Social development through reinstating sense support, giving and working together as a community.</li>
+                    </ul>
+                </header>
+            </div><!-- end of .col -->
+            <div class="col col-4">
+                <header class="section-header">
+                    <h4>Impact</h4>
+                    <ul>
+                        <li>We provide a connection between the people in need and the resources they need to make it and succeed in life</li>
+                        <li>We partner with the Government and Organisations to ensure effective positive impact on our nation</li>
+                        <li>We provide complementary education necessary for self-development and leadership in the nation</li>
+                        <li>We equip the nation with essential skills to thrive in the economy, in turn create jobs and employment</li>
+                        <li>Access to employment, including learnerships and internships </li>
+                        <li>We create an environment of social development where there is support, giving back and empowerment of all, especially the vulnerable and previously disadvantaged</li>
+                        <li>We capacitate and empower SMMES, NPOs, NGOs and Entrepreneurs with necessary support to succeed and thrive with the economy</li>
+                    </ul>
+                </header>
+            </div><!-- end of .col -->
+            <div class="col col-4">
+                <header class="section-header">
+                    <h4>Vision</h4>
+                    <p>To emancipate and empower the nation through restoration and building together.</p>
+                </header>
             </div><!-- end of .col -->
         </div><!-- end of .container -->
     </div><!-- end of .content -->
